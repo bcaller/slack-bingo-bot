477e747705861be9e24081c0a5f62c68fb57b681
JonasEtzold <jetzold@newstore.com>
2016-12-19 14:49:20 +0100

    feat(insult): adds support to insulting on incomplete sentences BIN-5

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 "use strict";
 
-var Botkit = require('botkit')
-    , giphy = require('./giphy')
-    , joinAndCommas = require('./joinAndComma')
-    , Promise = require('bluebird')
-    , readFile = Promise.promisify(require("fs").readFile)
-    , emoji = "boom"
-    , react = require('./react')
-    , noop = ()=>undefined
-    , PERSIST_BINGO_WORDS = process.env.PERSIST_BINGO_WORDS && process.env.PERSIST_BINGO_WORDS != 0
-    , botUid
-    , words = []
-    , foundWords = []
-    , persistence = require('./persistence')
-    , bot
-    , controller = Botkit.slackbot({
+var Botkit = require('botkit'),
+    giphy = require('./giphy'),
+    joinAndCommas = require('./joinAndComma'),
+    Promise = require('bluebird'),
+    readFile = Promise.promisify(require("fs").readFile),
+    emoji = "boom",
+    react = require('./react'),
+    insultgen = require('insultgenerator'),
+    noop = () => undefined,
+    PERSIST_BINGO_WORDS = process.env.PERSIST_BINGO_WORDS && process.env.PERSIST_BINGO_WORDS != 0,
+    botUid, words = [],
+    foundWords = [],
+    persistence = require('./persistence'),
+    bot, controller = Botkit.slackbot({
         logLevel: 3
-        //debug: true
-        //include "log: false" to disable logging
-        //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
-    })
+            //debug: true
+            //include "log: false" to disable logging
+            //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
+    }),
+    sentenceMatcher = new RegExp(`((?:[A-Z]\.|[^\.!?])+)[\.!?]`, "g");
 
 //---------------------------
 // Global functions
@@ -32,19 +32,35 @@ function safeRegex(s) {
 
 // hit matches hittings hitters hitting hitter hitted hited hiting hiter hiters hitings
 function makeRegex(s) {
-    if(/^\w.*\w$/i.test(s))
+    if (/^\w.*\w$/i.test(s))
         return new RegExp(`\\b${safeRegex(s)}${safeRegex(s[s.length-1])}?(ings?|ers?|ed|s)?\\b`, "i")
-    //Also work with :cow:
+            //Also work with :cow:
     let beginWordBreak = /^\b/.test(s) ? "\\b" : ""
     let endWordBreak = /\b$/.test(s) ? "\\b" : ""
     return new RegExp(beginWordBreak + safeRegex(s) + endWordBreak, "i")
+}
+
+function containsSentence(message) {
+    // Enforce minimum word count
+    if (message.split(/\s+/).length < 2) {
+        return false;
+    }
+    // Enforce the syntactic of a sentence
+    let sentences = message.match(sentenceMatcher);
+    return sentences ? true : false;
+}
+
+function validateMessage(message) {
+    if (!containsSentence(message)) {
+
+    }
 }
 
 function addWord(toAdd, addedBy) {
     toAdd = toAdd.trim().toLowerCase()
         .replace(/(^"|"$)/g, '') //Surrounding quotes
         .replace(/(,|;)\s/g, '') //Separators
-    //Check for duplicates
+        //Check for duplicates
     for (let w of words) {
         if (w.word == toAdd)
             return false
@@ -57,7 +73,7 @@ function addWord(toAdd, addedBy) {
     }
     words.push(word)
     return giphy.randomRetryFirstWord(word.word).then(data => {
-        if(data.fixed_height_downsampled_url) {
+        if (data.fixed_height_downsampled_url) {
             word.gif = data.fixed_height_downsampled_url
         }
     }).catch(noop)
@@ -67,8 +83,8 @@ function introduce(bot, message) {
     react(bot, 'thumbsup', message, noop)
     bot.reply(message, {
         text: 'Let\'s play BINGO:exclamation: Be the first to send any of my secret magic words and win:trophy:\n' +
-        `Suggest secret words by sending <@${botUid}> a private message:love_letter: starting with *add*\n` +
-        'e.g. `add :cake: "big data" landslide` to add :cake:, landslide and big data as bingo words',
+            `Suggest secret words by sending <@${botUid}> a private message:love_letter: starting with *add*\n` +
+            'e.g. `add :cake: "big data" landslide` to add :cake:, landslide and big data as bingo words',
         icon_emoji: `:${emoji}:`,
         username: 'bingo'
     });
@@ -83,21 +99,23 @@ function attachmentGif(w) {
 }
 
 function loadWords() {
-    persistence.load(function(remainingWords, bingoedWords, err){
+    persistence.load(function (remainingWords, bingoedWords, err) {
         if (err == undefined) {
             console.log('Loaded saved words successfully');
             foundWords = bingoedWords.filter(w => {
-                w.regExp= makeRegex(w.word);
+                w.regExp = makeRegex(w.word);
                 return true;
             });
             words = remainingWords.filter(w => {
-                w.regExp= makeRegex(w.word);
+                w.regExp = makeRegex(w.word);
                 return true;
             });
         } else {
             console.log("Load words from initial.txt");
             readFile('initial.txt', 'utf8').then(txt => {
-                Promise.map(txt.split(/[\n\r]+/), w => addWord(w), {concurrency: 5})
+                Promise.map(txt.split(/[\n\r]+/), w => addWord(w), {
+                    concurrency: 5
+                })
             })
         }
     });
@@ -105,7 +123,7 @@ function loadWords() {
 
 function exitHandler() {
     bot.destroy(bot);
-    persistence.save(words, foundWords, function(err) {
+    persistence.save(words, foundWords, function (err) {
         if (err) {
             throw err;
         } else {
@@ -124,7 +142,7 @@ controller.hears('add', ['direct_message'], (bot, message) => {
     message.text.match(/(?:"([^"]+)"|([^"\s]+))/g).splice(1).forEach(w => {
         if (addWord(w, message.user)) {
             bot.reply(message, {
-                text:'Added ' + w,
+                text: 'Added ' + w,
                 icon_emoji: `:${emoji}:`
             });
             react(bot, 'thumbsup', message, noop)
@@ -141,58 +159,72 @@ controller.hears(['hello', 'hi', 'introduce'], ['direct_message', 'direct_mentio
 
 controller.on('ambient', function ambient(bot, message) {
     if (message.type == 'message') {
-        //search text for bingo word
-        let found = []
-        words = words.filter(w => {
-            if (w.addedBy != message.user && w.regExp.test(message.text)) {
-                found.push(w)
 
-                return PERSIST_BINGO_WORDS
-            }
-            return true
-        })
-
-        var alreadyFound = foundWords.filter(fw => {
-            if (fw.regExp.test(message.text)) {
-                return true;
-            }
-
-            return false
-        });
-
-        if (found.length) {
-
-            found.map(f => {
-                foundWords.push({
-                    word: f.word,
-                    usr: message.user,
-                    regExp: f.regExp
+        // validate message
+        if (!containsSentence(message.text)) {
+            insultgen(function (insult) {
+                console.log(insult);
+                bot.reply(message, {
+                    username: 'bingo',
+                    text: `Sorry, that's not a sentence: ${insult}`,
+                    icon_emoji: `:anton:`
                 })
+            })
+        } else {
+
+            //search text for bingo word
+            let found = []
+            words = words.filter(w => {
+                if (w.addedBy != message.user && w.regExp.test(message.text)) {
+                    found.push(w)
+
+                    return PERSIST_BINGO_WORDS
+                }
+                return true
+            })
+
+            var alreadyFound = foundWords.filter(fw => {
+                if (fw.regExp.test(message.text)) {
+                    return true;
+                }
+
+                return false
             });
 
-            let foundStr = joinAndCommas(found.map(w => w.word))
-            react(bot, emoji, message, noop)
-            let poser = found.map(w => w.addedBy ? `<@${w.addedBy}>` : '').join(' ')
-            bot.reply(message, {
-                username: 'bingo',
-                text: `Bingo! <@${message.user}> said ${foundStr}! There are ${words.length || "no"} bingo words left to discover! ${poser}`,
-                icon_emoji: `:${emoji}:`,
-                attachments: found.filter(w => w.gif).map(attachmentGif)
-            })
-        }
+            if (found.length) {
 
-        if (alreadyFound.length) {
-            let foundStr = joinAndCommas(alreadyFound.map(w => {
-                if (w != undefined) {
-                    return w.word
-                }
-            }));
+                found.map(f => {
+                    foundWords.push({
+                        word: f.word,
+                        usr: message.user,
+                        regExp: f.regExp
+                    })
+                });
 
-            bot.reply(message, {
-                username: 'bingo',
-                text: `The following words were already bingoed: ${foundStr}`,
-                icon_emoji: `:${emoji}:`
-            })
+                let foundStr = joinAndCommas(found.map(w => w.word))
+                react(bot, emoji, message, noop)
+                let poser = found.map(w => w.addedBy ? `<@${w.addedBy}>` : '').join(' ')
+                bot.reply(message, {
+                    username: 'bingo',
+                    text: `Bingo! <@${message.user}> said ${foundStr}! There are ${words.length || "no"} bingo words left to discover! ${poser}`,
+                    icon_emoji: `:${emoji}:`,
+                    attachments: found.filter(w => w.gif).map(attachmentGif)
+                })
+            }
+
+            if (alreadyFound.length) {
+                let foundStr = joinAndCommas(alreadyFound.map(w => {
+                    if (w != undefined) {
+                        return w.word
+                    }
+                }));
+
+                bot.reply(message, {
+                    username: 'bingo',
+                    text: `The following words were already bingoed: ${foundStr}`,
+                    icon_emoji: `:${emoji}:`
+                })
+            }
         }
     }
 })
@@ -210,9 +242,12 @@ process.on('SIGINT', exitHandler);
 controller.spawn({
     token: process.env.SLACK_TOKEN
 }).startRTM((err, b, payload) => {
-    if(err) throw err
+    if (err) throw err
     botUid = payload.self.id
-    bot= b;
+    bot = b;
 })
 
 loadWords();
+
+// Hack: Make functions available for testing
+exports.isSentence = isSentence;
